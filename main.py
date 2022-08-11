2b17b8ae661c3936b42cc708f37e0c9884c1c806
Naufal Fathirachman Mahing <71597476+naufalmahing@users.noreply.github.com>
2022-08-11 09:21:28 +0700

    uncomment load model and add sklearn modules

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import pickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
 
 st.header('Tes deploy model ke streamlit sebagai dasar buat integrasi model ke aplikasi')
 st.text_input("Enter your name: ", key="name")
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/iris-data.csv')
 
 # load model
-# filename = 'model.sav'
-# loaded_model = pickle.load(open(filename, 'rb'))
+filename = 'model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
 
 # show dataframe
 if st.checkbox('Show dataframe'):
